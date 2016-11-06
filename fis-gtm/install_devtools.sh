fa7c10e833347e349293f2c13b2b9cf1419f5272
Timur Safin <timur.safin@gmail.com>
2016-11-06 23:58:43 +0300

    Created development version of fis-gtm container with all packages preinstalled

---
+++ fis-gtm/install_devtools.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y man info git
+apt-get build-dep -y fis-gtm
+# apt-get install -y `apt-rdepends -b --FOLLOW=depends --state-show=NotInstalled fis-gtm | \
+#	perl -ane 'print "$F[1]\n";'`
