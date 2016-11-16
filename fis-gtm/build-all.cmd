11aa0ee79ee117ace17bd2da6afdb27d5e6881ca
Timur Safin <timur.safin@gmail.com>
2016-11-16 16:36:21 +0300

    Add gdb and strace to the developers image

---
+++ fis-gtm/build-all.cmd
@@ -0,0 +1,2 @@
+: docker build -t fis-gtm:ubuntu14.04 .
+docker build -t fis-gtm-devel:ubuntu14.04 -f Dockerfile.buildgtm .
--- fis-gtm/install_devtools.sh
+++ fis-gtm/install_devtools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y man info git
+apt-get install -y man info git gdb strace
 apt-get build-dep -y fis-gtm
 # apt-get install -y `apt-rdepends -b --FOLLOW=depends --state-show=NotInstalled fis-gtm | \
 #	perl -ane 'print "$F[1]\n";'`
